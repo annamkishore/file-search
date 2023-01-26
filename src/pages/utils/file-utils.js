4cd5e580d00c6ac59e019ca9234f7f027f0431e7
Krishna Annam <annamkishore@gmail.com>
2023-01-27 01:23:58 +0530

    feature: max line length

--- src/pages/utils/file-utils.js
+++ src/pages/utils/file-utils.js
@@ -1,4 +1,6 @@
 const fs = require("fs").promises
+const fs1 = require("fs")
+const path = require("path")
 
 let space = count => Array(2 * count).fill("-").join("")
 
@@ -10,7 +12,8 @@ async function print(dir) {
   search = {
     dir_test: "/media/kishore/Work/__git/my-work-docs/cncf",
     dir,
-    ignoreDirs: ["node_modules", ".next", ".git", ".idea", "output", "vendor", "resources"]
+    ignoreDirs: ["node_modules", ".next", ".git", ".idea", "output", "vendor", "resources", "bind_linux_amd64.go"],
+    needDetails: [".go"]
   }
   result = []
   return await listFiles(search.dir)
@@ -31,12 +34,16 @@ async function listFiles(dir) {
       const ignore = search.ignoreDirs.includes(file.name)
 
       // 3. file - print it
-      let size = `(${(file.stat.size/1024).toFixed(1)} kb)`
-      let temp = `${space(count)} ${i + 1} ${file.name} ${isDir ? "" : size} ${ignore ? " (ignored..)" : ""}`;
+      const maxLineLen = !search.ignoreDirs.includes(file.name) ? await getMaxLineLength(file) : ""
+      const max = 40
+      let size = `${(file.stat.size/1024).toFixed(1)} kb`.padStart(10)
+      let temp = `${space(count)} ${i + 1} ${file.name} ${ignore ? "⛔" : ""}`
+      temp = temp.length > max ? temp.slice(0, max-1) + temp.at(-1) : temp.padEnd(max)
+      temp += `${isDir ? "" : size} ${maxLineLen}`
       result.push(temp)
 
-      if(ignore){
-        continue;
+      if(ignore){ // ignoring directory contents to process
+        continue
       }
 
       // 4. dir - traverse it
@@ -53,6 +60,37 @@ async function listFiles(dir) {
     throw err
   }
 }
+
+async function getMaxLineLength(file) {
+  let ext = path.extname(file.name)
+  if(!ext || !search.needDetails.includes(ext)) {
+    return ""
+  }
+
+  return await processLineByLine(file.nameWithPath)
+}
+
+const readline = require('readline');
+
+async function processLineByLine(fileName) {
+  const fileStream = fs1.createReadStream(fileName);
+
+  const rl = readline.createInterface({input: fileStream, crlfDelay: Infinity});
+
+  let maxLineLength = 0
+  for await (const line of rl) {
+    // console.log(`Line from file: ${line}`);
+    if(maxLineLength < line.length) {
+      maxLineLength = line.length
+      if(maxLineLength > 2200) {
+        console.log(line)
+      }
+    }
+  }
+
+  return maxLineLength
+}
+
 
 module.exports = {
   print
